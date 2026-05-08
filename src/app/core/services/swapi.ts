fe216a51c850563eac2a1ce80ee45075fc7b7c7c
pretomhira <pretomdas7.pd@gmail.com>
2026-05-08 23:06:21 +0200

    ref: used rxjs for http request

--- src/app/core/services/swapi.ts
+++ src/app/core/services/swapi.ts
@@ -1,6 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { Starship } from '../models/starship.model';
+
+interface SwapiPaginatedResponse<T> {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: T[];
+}
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +18,7 @@ export class Swapi {
   private http = inject(HttpClient);
   private readonly API_URL = environment.apiUrl;
 
-  getStarships(pageNumber: number) {
-    return this.http.get(`${this.API_URL}/starships/?page=${pageNumber}`);
+  getStarships(pageNumber: number): Observable<SwapiPaginatedResponse<Starship> | Starship[]> {
+    return this.http.get<SwapiPaginatedResponse<Starship> | Starship[]>(`${this.API_URL}/starships/?page=${pageNumber}`);
   }
 }
