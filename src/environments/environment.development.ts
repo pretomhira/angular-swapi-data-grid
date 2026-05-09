7f620044e8cf462264746552414054f7d9e9968a
pretomhira <pretomdas7.pd@gmail.com>
2026-05-09 23:14:04 +0200

    fix: need to change api url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   environmentName: 'development',
-  apiUrl: 'https://swapi.info/api',
+  apiUrl: 'https://rickandmortyapi.com/api',
   enableLogging: true,
 };
